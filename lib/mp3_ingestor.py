da067ac827081e0402652521145b6234e4241480
mwang316 <mwang1337@gmail.com>
2020-12-24 16:27:07 -0500

    more changes

--- lib/mp3_ingestor.py
+++ lib/mp3_ingestor.py
@@ -1,6 +1,9 @@
 import librosa
 
 filename = "C:\\Users\\mwang\\sheet_music_project\\music\\fur_elise\\fur_elise.mp3"
+# y, sr = librosa.load(filename)
+# filename = librosa.util.example_audio_file()
+print(filename)
 y, sr = librosa.load(filename)
 tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
 print('Estimated tempo: {:.2f} beats per minute'.format(tempo))
